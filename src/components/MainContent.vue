860798381ba3d74dc4d5b927cefcd8880cb82fec
김해성 <n0n0ne@gimhaeseong-ui-MacBookPro.local>
2022-06-25 20:01:24 +0900

    레이아웃 구현

---
+++ src/components/MainContent.vue
@@ -0,0 +1,40 @@
+<template>
+  <h2 class="content-title">{{ content.label }}</h2>
+  <component :is="targetComponent"></component>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+export default {
+  name: "MainContent",
+  props: {
+    content: {
+      type: Object,
+      default() {
+        return {
+          label: "TEST Menu",
+          path: "test",
+          index: "testMenu",
+        };
+      },
+    },
+  },
+  data() {
+    return {
+      targetComponent: null,
+    };
+  },
+  watch: {
+    content: {
+      deep: true,
+      handler: function (newValue) {
+        this.targetComponent = defineAsyncComponent(() =>
+          import(`@/components/${newValue.path}/${newValue.index}`)
+        );
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// element plus
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+// grobal css
+import "@/styles/main.css";
+
+// createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(ElementPlus);
+
+app.mount("#app");
